eaf35512d0168128086cfbcffe90a9a1464d9a47
Toby Kreiselmaier <toby.kreiselmaier@energi.team>
2022-10-09 14:00:57 -0400

    Updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-waffle');
 
-const INFURA_URL = 'https://goerli.infura.io/v3/c2f69015a46346a3a1beb31b48cdac2b'
-const PRIVATE_KEY = '96371452a0febcd710247607113c2805f7bd9256f166cec0219d6f6721f83a6b'
+const INFURA_URL = 'https://goerli.infura.io/v3/...'
+const PRIVATE_KEY = ''
 
 module.exports = {
   solidity: '0.8.17',
